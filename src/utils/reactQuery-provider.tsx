530375acb344f22194a8fe2bbaef0899ce85e051
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-06-01 18:37:41 +0530

    made the trpc production ready

--- src/utils/reactQuery-provider.tsx
+++ src/utils/reactQuery-provider.tsx
@@ -1,15 +1,27 @@
 // lib/reactQuery-provider.tsx
- 
-'use client';
- 
-import React, { ReactNode, useState } from 'react';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
-import superjson from 'superjson';
-import { trpc } from '../../trpc-client/client';
- 
-const url = 'http://localhost:3000/api/trpc';
- 
+
+"use client";
+
+import React, { ReactNode, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import superjson from "superjson";
+import { trpc } from "../../trpc-client/client";
+
+//check wheather the url is form vercel or localhost
+function getBaseUrl() {
+  if (typeof window !== "undefined")
+    // browser should use relative path
+    return "";
+  if (process.env.VERCEL_URL)
+    // reference for vercel.com
+    return `https://${process.env.VERCEL_URL}`;
+  if (process.env.RENDER_INTERNAL_HOSTNAME)
+    // reference for render.com
+    return `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}`;
+  // assume localhost
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
 export const TrpcProvider = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(
     () =>
@@ -21,16 +33,16 @@ export const TrpcProvider = ({ children }: { children: ReactNode }) => {
         },
       })
   );
- 
+
   const trpcClient = trpc.createClient({
     transformer: superjson,
     links: [
       httpBatchLink({
-        url: url,
+        url: `${getBaseUrl()}/api/trpc`,
       }),
     ],
   });
- 
+
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
