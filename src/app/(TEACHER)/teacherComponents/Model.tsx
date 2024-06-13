0f8de49980ad65f69b7fb32ab4433a25ba3e2cae
Darshil Mahraur <darshilmahraur67@gmail.com>
2024-06-13 20:46:28 +0530

    updated the attendance ui

--- src/app/(TEACHER)/teacherComponents/Model.tsx
+++ src/app/(TEACHER)/teacherComponents/Model.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, { useState, ChangeEvent, FormEvent } from "react";
 import { trpc } from "../../../../trpc-client/client";
+import { RxCross1 } from "react-icons/rx";
+import Image from "next/image";
 
 interface ModalProps {
   handleClose: () => void;
@@ -38,8 +40,8 @@ const Modal: React.FC<ModalProps> = ({ handleClose, courseId, classId }) => {
           }
         );
         const uploadImageData = await uploadResponse.json();
-        console.log(uploadImageData);
         setUploadedImageUrls(uploadImageData.secure_url);
+        console.log(uploadImageData.secure_url);
       } catch (error) {
         console.error("Error uploading image:", error);
       }
@@ -73,14 +75,13 @@ const Modal: React.FC<ModalProps> = ({ handleClose, courseId, classId }) => {
   };
 
   const handleUpload = async (
-    e : FormEvent<HTMLFormElement>,
-   courseId: string,
+    e: FormEvent<HTMLFormElement>,
+    courseId: string,
     classId: string
   ) => {
     e.preventDefault();
     if (uploadedImageUrls) {
       // Handle file upload logic here
-      console.log("ja rha hai");
 
       console.log(uploadedImageUrls);
 
@@ -92,8 +93,6 @@ const Modal: React.FC<ModalProps> = ({ handleClose, courseId, classId }) => {
           },
           body: JSON.stringify({ link: uploadedImageUrls }),
         }).then((res) => res.json());
-
-        console.log(res);
 
         // convert this res array to object to array of string
         // getting the names of the students from ai model and storing it in the answers array
@@ -131,35 +130,83 @@ const Modal: React.FC<ModalProps> = ({ handleClose, courseId, classId }) => {
             className="text-gray-600 hover:text-gray-900"
             onClick={handleClose}
           >
-            &times;
+            <RxCross1 size={25} />
           </button>
         </div>
-        <form onSubmit={(e)=> handleUpload (e, courseId, classId )}>
-          <label className="text-gray-700 font-medium">Upload Image</label>
-          <input
-            type="file"
-            className="border border-gray-300 p-2 w-full mb-4"
-            accept="image/*"
-            onChange={handleFileChange}
-          />
+        <form onSubmit={(e) => handleUpload(e, courseId, classId)}>
+          {!imageUrl && (
+            <>
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="file_input"
+              >
+                Upload Attendance Image
+              </label>
+              <input
+                className="block w-full text-sm text-gray-900 border mb-2 p-2 border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                aria-describedby="file_input_help"
+                id="file_input"
+                type="file"
+                accept="image/*"
+                onChange={handleFileChange}
+              />
+            </>
+          )}
+
           {imageUrl && (
             <div className="mt-4 mb-4">
-              <img
+              <Image
                 src={imageUrl as string}
                 alt="Uploaded"
-                className="max-w-full h-auto"
+                width={200}
+                height={200}
               />
             </div>
           )}
 
-          <label className="text-gray-700 font-medium">Upload Notes</label>
-          <input
-            type="file"
-            className="border border-gray-300 p-2 w-full mb-4"
-            placeholder="Upload Notes"
-            accept="application/pdf"
-            onChange={handleNotesChange}
-          />
+          {!localNotesUrl && (
+            <>
+              <div className="flex items-center justify-center w-full mb-5">
+                <label
+                  htmlFor="dropzone-file"
+                  className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                >
+                  <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                    <svg
+                      className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 16"
+                    >
+                      <path
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                      />
+                    </svg>
+                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                      <span className="font-semibold">Click to upload </span>
+                      notes
+                    </p>
+                    <p className="text-xs text-gray-500 dark:text-gray-400">
+                      PDF (MAX. 100 MB)
+                    </p>
+                  </div>
+                  <input
+                    id="dropzone-file"
+                    type="file"
+                    className="hidden"
+                    accept="application/pdf"
+                    onChange={handleNotesChange}
+                  />
+                </label>
+              </div>
+            </>
+          )}
+
           {localNotesUrl && (
             <embed
               src={localNotesUrl}
@@ -172,7 +219,7 @@ const Modal: React.FC<ModalProps> = ({ handleClose, courseId, classId }) => {
 
           <button
             type="submit"
-            className="bg-blue-500 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded w-full hover:bg-blue-700 transition duration-200"
           >
             Upload
           </button>
